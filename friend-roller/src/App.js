59de411dabf3c453b3424a2d1ba0a081b5106bd5
waterFish2604 <adityawankhede.and21@gmail.com>
2024-09-14 15:16:28 +0530

    objects is js and ho react uses them

--- friend-roller/src/App.js
+++ friend-roller/src/App.js
@@ -1,25 +1,33 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Aditya"
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi! My name is {this.state.name}
+          </p>
+          <button className='name-changer' onClick={() => {
+            this.setState({name: "WaterFish"})
+          }}>Change Name</button>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
