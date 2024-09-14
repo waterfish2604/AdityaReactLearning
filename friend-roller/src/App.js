9cb32ff28b3d80a4e73acb14b3317728d4b6f75d
waterFish2604 <adityawankhede.and21@gmail.com>
2024-09-14 23:13:31 +0530

    api call and lifecycle

--- friend-roller/src/App.js
+++ friend-roller/src/App.js
@@ -27,6 +27,15 @@ class App extends Component {
 
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return { friends: users };
+      }
+      ))
+  }
+
   render() {
     return (
       <div className="App">
